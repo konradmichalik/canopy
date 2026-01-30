274a2b22a5cd08f9082122d9eadd7223f2bbb9df
Konrad Michalik <km@move-elevator.de>
2026-01-30 15:22:10 +0100

    style: adjust FlashMessage component layout and icon sizes for improved aesthetics

--- src/lib/components/common/FlashMessage.svelte
+++ src/lib/components/common/FlashMessage.svelte
@@ -10,15 +10,15 @@
 
 {#if message}
   <div
-    class="fixed bottom-4 right-4 px-4 py-3 rounded-md shadow-sm z-50 flex items-center gap-2 animate-slide-up border bg-background
+    class="fixed bottom-6 right-6 px-3 py-2 rounded-md shadow-sm z-50 flex items-center gap-1.5 animate-slide-up border bg-background text-xs
       {message.type === 'success'
       ? 'border-chart-2 text-chart-2'
       : 'border-destructive text-destructive'}"
   >
     {#if message.type === 'success'}
-      <AtlaskitIcon name="check" size={20} />
+      <AtlaskitIcon name="check" size={16} />
     {:else}
-      <AtlaskitIcon name="cross" size={20} />
+      <AtlaskitIcon name="cross" size={16} />
     {/if}
     <span>{message.text}</span>
   </div>
